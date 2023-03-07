de1054492cb312705a7f5537b62b0085a012dbbe
Daniel amos <dodgydan2001@hotmail.com>
2023-03-06 21:49:57 -0500

    Added DALL-E calls to generate 4 images of the prompt. Added DownloadFile call to each url generated. Added call to open windows explorer to directory of downloaded pictures.

--- Program.cs
+++ Program.cs
@@ -3,6 +3,10 @@ using OpenAI.GPT3.Managers;
 using OpenAI.GPT3;
 using OpenAI.GPT3.ObjectModels.RequestModels;
 using OpenAI.GPT3.ObjectModels;
+using System.Net;
+using System;
+using System.Diagnostics;
+using System.IO;
 
 Console.WriteLine("Hello, ChatGPT world!");
 
@@ -34,5 +38,44 @@ if (completionResult.Successful)
 }
 else
 {
-    Console.WriteLine(completionResult.Error.ToString());
+    Console.WriteLine($"{completionResult.Error.Code}: {completionResult.Error.Message}");
+}
+
+
+var imageResult = await openAiService.Image.CreateImage(new ImageCreateRequest
+{
+    Prompt = "Flying super dog against a blue sky with clouds and sun",
+    N = 4,
+    Size = StaticValues.ImageStatics.Size.Size1024,
+    ResponseFormat = StaticValues.ImageStatics.ResponseFormat.Url,
+    User = "TestUser"
+});
+
+
+if (imageResult.Successful)
+{
+    using (WebClient client = new WebClient())
+    {
+        int i = 0;
+        foreach(var u in imageResult.Results)
+        {
+            //cheesy way of making the filenames differ
+            var filename = @"image-" + DateTime.Now.ToShortDateString().Replace('/', '-') + "-" + DateTime.Now.ToLongTimeString().Replace(':','-') + ".png";
+
+            //download the output image to the working directory
+            client.DownloadFile(new Uri(u.Url), filename);
+            
+            //ghetto way of making sure the file name is different due to using timestamps
+            System.Threading.Thread.Sleep(1000);
+        }        
+    }
+
+    //display the file urls
+    Console.WriteLine(string.Join("\n\n", imageResult.Results.Select(r => r.Url)));
+
+    //open explorer to see the output files
+    Process.Start("explorer.exe", "/open, " + new Uri(
+    System.IO.Path.GetDirectoryName(
+        System.Reflection.Assembly.GetExecutingAssembly().CodeBase)
+    ).LocalPath);
 }
--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # ChatGPT_API_Test_Console_App
 
+A simple console app that sends your question or statement to ChatGPT for processing and it then prints out the result.
+OpenAi doesnt have an official C# SDK, however this is their suggested SDK, therefore it was leveraged in this console app.
+https://github.com/betalgo/openai
+
 The app expects a secrets.json with the following secret (Replace the API key with yours):
 
 {
