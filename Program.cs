09e589ebc14a613107ed45dace0d01b564ae00a8
Daniel amos <dodgydan2001@hotmail.com>
2023-03-07 19:47:16 -0500

    menu loop logic. Tweaked DALL-E logic to allow multiple images. Readme update.

--- Program.cs
+++ Program.cs
@@ -4,78 +4,175 @@ using OpenAI.GPT3;
 using OpenAI.GPT3.ObjectModels.RequestModels;
 using OpenAI.GPT3.ObjectModels;
 using System.Net;
-using System;
 using System.Diagnostics;
-using System.IO;
+using System.Reflection;
+using OpenAI.GPT3.ObjectModels.SharedModels;
+using OpenAI.GPT3.Interfaces;
+using OpenAI.GPT3.ObjectModels.ResponseModels.ImageResponseModel;
 
-Console.WriteLine("Hello, ChatGPT world!");
-
-var config = new ConfigurationBuilder().AddUserSecrets<Program>().Build();
-
-var secretProvider = config.Providers.First();
-secretProvider.TryGet("ChatGPT:ServiceApiKey", out var secretPass);
-
-var openAiService = new OpenAIService(new OpenAiOptions()
+namespace ChatGPT_API_Test_Console_App
 {
-    ApiKey = secretPass
-});
-
-
-
-var completionResult = await openAiService.ChatCompletion.CreateCompletion(new ChatCompletionCreateRequest
-{
-    Messages = new List<ChatMessage>
+    internal class Program
     {
-        ChatMessage.FromUser("Who won the last FIFA world cup?")
-    },
-    Model = Models.ChatGpt3_5Turbo,
-    MaxTokens = 50//optional
-});
-
-if (completionResult.Successful)
-{
-    Console.WriteLine(completionResult.Choices.First().Message.Content);
-}
-else
-{
-    Console.WriteLine($"{completionResult.Error.Code}: {completionResult.Error.Message}");
-}
-
-
-var imageResult = await openAiService.Image.CreateImage(new ImageCreateRequest
-{
-    Prompt = "Flying super dog against a blue sky with clouds and sun",
-    N = 4,
-    Size = StaticValues.ImageStatics.Size.Size1024,
-    ResponseFormat = StaticValues.ImageStatics.ResponseFormat.Url,
-    User = "TestUser"
-});
-
-
-if (imageResult.Successful)
-{
-    using (WebClient client = new WebClient())
-    {
-        int i = 0;
-        foreach(var u in imageResult.Results)
+        static async Task Main(string[] args)
         {
-            //cheesy way of making the filenames differ
-            var filename = @"image-" + DateTime.Now.ToShortDateString().Replace('/', '-') + "-" + DateTime.Now.ToLongTimeString().Replace(':','-') + ".png";
+            bool exit = false;
 
-            //download the output image to the working directory
-            client.DownloadFile(new Uri(u.Url), filename);
-            
-            //ghetto way of making sure the file name is different due to using timestamps
-            System.Threading.Thread.Sleep(1000);
-        }        
+            var config = new ConfigurationBuilder().AddUserSecrets<Program>().Build();
+
+            var secretProvider = config.Providers.First();
+            secretProvider.TryGet("ChatGPT:ServiceApiKey", out var secretPass);
+
+            var openAiService = new OpenAIService(new OpenAiOptions()
+            {
+                ApiKey = secretPass
+            });
+
+            while (!exit)
+            {
+                switch (Program.ShowMenu())
+                {
+                    case 0: 
+                        Console.WriteLine("");
+                        exit = true;
+                        break;
+                    case 1: 
+                        Console.WriteLine("");
+                        await PromptChatGpt(openAiService);
+                        break;
+                    case 2: 
+                        Console.WriteLine("");
+                        await PromptDalle(openAiService);
+                        break;
+                }
+            }
+        }
+
+        private static async Task PromptDalle(IOpenAIService service, string prompt = "Flying super dog against a blue sky with clouds and sun", int imageCount = 2)
+        {
+            Console.WriteLine("Current prompt: " + prompt);
+            Console.WriteLine("Enter prompt: ");
+
+            var input = Console.ReadLine().ToLower();
+            if (!string.IsNullOrEmpty(input))
+            {
+                prompt = input;
+            }
+
+            Console.WriteLine("Current Image generation count: " + imageCount);
+            Console.WriteLine("Enter to continue, or type the desired amount of images (max is 10): ");
+
+            input = Console.ReadLine().ToLower();
+            if (!string.IsNullOrEmpty(input))
+            {
+                imageCount = int.Parse(input);
+                if (imageCount > 10)
+                {
+                    imageCount = 10;
+                }
+                else if (imageCount < 1)
+                {
+                    imageCount = 1;
+                }
+            }
+            else
+            {
+                imageCount = 2;
+            }
+
+            var imageResult = await service.Image.CreateImage(new ImageCreateRequest
+            {
+                Prompt = prompt,
+                N = imageCount,
+                Size = StaticValues.ImageStatics.Size.Size1024,
+                ResponseFormat = StaticValues.ImageStatics.ResponseFormat.Url,
+                User = "TestUser"
+            });
+
+
+            if (imageResult.Successful)
+            {
+                using (WebClient client = new WebClient())
+                {
+                    int i = 0;
+                    foreach (var u in imageResult.Results)
+                    {
+                        //cheesy way of making the filenames differ
+                        var filename = @"image-" + DateTime.Now.ToShortDateString().Replace('/', '-') + "-" + DateTime.Now.ToLongTimeString().Replace(':', '-') + ".png";
+
+                        //download the output image to the working directory
+                        client.DownloadFile(new Uri(u.Url), filename);
+
+                        //ghetto way of making sure the file name is different due to using timestamps
+                        System.Threading.Thread.Sleep(1000);
+                    }
+                }
+
+                //display the file urls
+                Console.WriteLine(string.Join("\n\n", Enumerable.Select<ImageCreateResponse.ImageDataResult, string>(imageResult.Results, r => r.Url)));
+
+                //open explorer to see the output files
+                var uriString = Path.GetDirectoryName(path: Assembly.GetExecutingAssembly().CodeBase);
+                _ = Process.Start("explorer.exe", "/open, " + new Uri(uriString).LocalPath);
+
+                Console.WriteLine("\nPress any key to continue.");
+                Console.ReadLine();
+            }
+        }
+
+        private static async Task PromptChatGpt(IOpenAIService service, string prompt = "Who won the last three FIFA world cups?")
+        {
+            Console.WriteLine("Current prompt: " + prompt);
+            Console.WriteLine("Enter prompt: ");
+
+            var input = Console.ReadLine().ToLower();
+            if (!string.IsNullOrEmpty(input))
+            {
+                prompt = input;
+            }
+
+            var completionResult = await service.ChatCompletion.CreateCompletion(new ChatCompletionCreateRequest
+            {
+                Messages = new List<ChatMessage>
+                {
+                    ChatMessage.FromUser(prompt)
+                },
+                Model = Models.ChatGpt3_5Turbo,
+                MaxTokens = 150 //optional
+            });
+
+            Console.WriteLine(completionResult.Successful ? Enumerable.First<ChatChoiceResponse>(completionResult.Choices).Message.Content : $"{completionResult.Error.Code}: {completionResult.Error.Message}");
+
+            Console.WriteLine("\nPress any key to continue.");
+            Console.ReadLine();
+        }
+
+        private static int ShowMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("Hello, ChatGPT world!");
+            Console.WriteLine("==================================================");
+            Console.WriteLine("Menu:                                             ");
+            Console.WriteLine("1 - Ask ChatGPT a question.                       ");
+            Console.WriteLine("2 - Ask DALL-E to generate image(s) from a prompt.");
+            Console.WriteLine("Q - Quit.                                         ");
+            Console.WriteLine("==================================================");
+            Console.WriteLine("Enter Choice: ");
+
+            var input = Console.ReadLine().ToLower();
+
+            if (input.Contains('q'))
+            {
+                return 0;
+            }
+            else if (input.Contains('1') || input.Contains('2'))
+            {
+                return int.Parse(input);
+            }
+
+            ShowMenu();
+
+            return 0;
+        }
     }
-
-    //display the file urls
-    Console.WriteLine(string.Join("\n\n", imageResult.Results.Select(r => r.Url)));
-
-    //open explorer to see the output files
-    Process.Start("explorer.exe", "/open, " + new Uri(
-    System.IO.Path.GetDirectoryName(
-        System.Reflection.Assembly.GetExecutingAssembly().CodeBase)
-    ).LocalPath);
 }
--- README.md
+++ README.md
@@ -1,8 +1,19 @@
-# ChatGPT_API_Test_Console_App
+# ChatGPT API Test Console App
 
-A simple console app that sends your question or statement to ChatGPT for processing and it then prints out the result.
-OpenAi doesnt have an official C# SDK, however this is their suggested SDK, therefore it was leveraged in this console app.
+A simple console app that lets you make two simple choices.
+The first sends your question or statement to ChatGPT for processing and it then prints out the result.
+The second uses a prompt to generate the specified number of images based on your prompt, these are saved to the executables folder and windows explorer is opened automatically so you may preview them since this is a console app.
+
+OpenAi doesnt have an official C# SDK, however Betalgo.OpenAI.GPT3 is their suggested SDK, therefore it was leveraged in this console app.
+[![Betalgo.OpenAI.GPT3](https://img.shields.io/nuget/v/Betalgo.OpenAI.GPT3?style=for-the-badge)](https://www.nuget.org/packages/Betalgo.OpenAI.GPT3/)
 https://github.com/betalgo/openai
+
+
+Your API Key comes from here --> https://platform.openai.com/account/api-keys
+
+API overview --> https://platform.openai.com/docs/introduction/overview
+
+
 
 The app expects a secrets.json with the following secret (Replace the API key with yours):
 
